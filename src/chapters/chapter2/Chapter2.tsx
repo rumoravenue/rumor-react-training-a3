ca5271a12ea24d983c7893a5be3ebdc5f1d519aa
Sandeep <sandeep34@outlook.com>
2024-04-12 17:18:23 -0700

    update provider

--- src/chapters/chapter2/Chapter2.tsx
+++ src/chapters/chapter2/Chapter2.tsx
@@ -1,7 +1,18 @@
 import { Box } from '@chakra-ui/react'
+import { SampleProvider, useSampleContext } from './SampleProvider'
 
 const Chapter2 = () => {
-  return <Box></Box>
+  const { sampleProp } = useSampleContext()
+  return <Box>{sampleProp} coming from Provider</Box>
 }
 
-export default Chapter2
+/** Since chapter 2 need to use the context, provider should be in the parent of the Chapter 2 */
+export const Chapter2WithProvider = () => {
+  return (
+    <SampleProvider>
+      <Chapter2 />
+    </SampleProvider>
+  )
+}
+
+export default Chapter2WithProvider
